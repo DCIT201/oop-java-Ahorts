bef56eb8ef5da660cbddc4ff8f514dfc7000ac17
Ahorts <godswayahorts@gmail.com>
2024-12-20 11:28:38 +0000

    feat: Implement class

--- AdvancedVMS/src/main/java/Motorcycle.java
+++ AdvancedVMS/src/main/java/Motorcycle.java
@@ -1,2 +1,12 @@
-public class Motorcycle {
+public class Motorcycle extends Vehicle {
+
+    public Motorcycle(String vehicleId, String model, double baseRentalRate){
+        super(vehicleId, model, baseRentalRate);
+    }
+    @Override
+    public double calculateRent(int days) {
+        return days * super.getBaseRentalRate();
+    }
+
+
 }

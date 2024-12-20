ab6c0d3b1baffe066a7ace5a59812208a4e1ea19
Ahorts <godswayahorts@gmail.com>
2024-12-20 15:26:44 +0000

    feat: Add rental method and  validation

--- AdvancedVMS/src/main/java/Customer.java
+++ AdvancedVMS/src/main/java/Customer.java
@@ -4,14 +4,17 @@ import java.util.List;
 public class Customer {
     private String customerId;
     private String name;
-    private String contact;
+    private String email;
+    private List<Vehicle> currentRentals;
     private final List<Vehicle> rentalHistory;
+    private boolean canRent = true;
 
-    public Customer(String customerId, String name, String contact) {
+    public Customer(String customerId, String name, String email) {
         this.customerId = customerId;
         this.name = name;
-        this.contact = contact;
+        this.email = email;
         rentalHistory = new ArrayList<>();
+        currentRentals = new ArrayList<>();
     }
     public String getCustomerId() {
         return customerId;
@@ -19,8 +22,8 @@ public class Customer {
     public String getName() {
         return name;
     }
-    public String getContact() {
-        return contact;
+    public String getEmail() {
+        return email;
     }
     public void setCustomerId(String customerId) {
         this.customerId = customerId;
@@ -28,8 +31,8 @@ public class Customer {
     public void setName(String name) {
         this.name = name;
     }
-    public void setContact(String contact) {
-        this.contact = contact;
+    public void setEmail(String email) {
+        this.email = email;
     }
     public void addToHistory(Vehicle vehicle) {
         rentalHistory.add(vehicle);
@@ -37,4 +40,31 @@ public class Customer {
     public List<Vehicle> getRentalHistory() {
         return rentalHistory;
     }
+
+    public void addRentals(Vehicle vehicle) {
+        if (!canRent) throw new IllegalStateException("Customer has reached the maximum number of rentals");
+        currentRentals.add(vehicle);
+        rentalHistory.add(vehicle);
+    }
+    public List<Vehicle> getCurrentRentals() {
+        return currentRentals;
+    }
+
+    public void setCanRent(boolean canRent) {
+        this.canRent = canRent;
+    }
+    public void setCanRent() {
+        this.canRent = currentRentals.size() < 4;
+    }
+
+    public boolean getCanRent() {
+        return canRent;
+    }
+
+    public void returnVehicle(Vehicle vehicle) {
+        if (!currentRentals.contains(vehicle)) throw new IllegalArgumentException("Customer has not rented this vehicle");
+        currentRentals.remove(vehicle);
+    }
+
 }
+

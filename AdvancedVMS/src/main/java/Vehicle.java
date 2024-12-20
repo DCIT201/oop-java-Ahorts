9054a0d6952aebe9b5ec01bcd0fbda23f400ca62
Ahorts <godswayahorts@gmail.com>
2024-12-20 10:16:48 +0000

    feat: create vehicle class

--- AdvancedVMS/src/main/java/Vehicle.java
+++ AdvancedVMS/src/main/java/Vehicle.java
@@ -1,2 +1,41 @@
-public class Vehicle {
+public abstract class Vehicle {
+    private String vehicleId;
+    private String model;
+    private double baseRentalRate;
+    private boolean isAvailable;
+
+    public Vehicle(String vehicleId, String model, double baseRentalRate) {
+        this.vehicleId = vehicleId;
+        this.model = model;
+        this.baseRentalRate = baseRentalRate;
+        this.isAvailable = false;
+    }
+    public String getVehicleId() {
+        return vehicleId;
+    }
+    public String getModel() {
+        return model;
+    }
+    public double getBaseRentalRate() {
+        return baseRentalRate;
+    }
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+    public void setVehicleId(String vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setBaseRentalRate(double baseRentalRate) {
+        this.baseRentalRate = baseRentalRate;
+    }
+
+    public abstract double calculateRent(int days);
+
 }

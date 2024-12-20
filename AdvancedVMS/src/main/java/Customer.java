51945217068f4427f2deae8ce001ebde123c6a3d
Ahorts <godswayahorts@gmail.com>
2024-12-20 13:31:22 +0000

    feat: Implement vehicle history

--- AdvancedVMS/src/main/java/Customer.java
+++ AdvancedVMS/src/main/java/Customer.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
+    private String customerId;
+    private String name;
+    private String contact;
+    private final List<Vehicle> rentalHistory;
+
+    public Customer(String customerId, String name, String contact) {
+        this.customerId = customerId;
+        this.name = name;
+        this.contact = contact;
+        rentalHistory = new ArrayList<>();
+    }
+    public String getCustomerId() {
+        return customerId;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getContact() {
+        return contact;
+    }
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+    public void addToHistory(Vehicle vehicle) {
+        rentalHistory.add(vehicle);
+    }
+    public List<Vehicle> getRentalHistory() {
+        return rentalHistory;
+    }
 }

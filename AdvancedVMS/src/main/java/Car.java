cf3b8135e26ae55e96b01dc75a217cbeda4e9b2f
Ahorts <godswayahorts@gmail.com>
2024-12-20 10:44:42 +0000

    feat: Implement car class

--- AdvancedVMS/src/main/java/Car.java
+++ AdvancedVMS/src/main/java/Car.java
@@ -1,2 +1,10 @@
-public class Car {
+public class Car extends Vehicle {
+    public Car(String vehicleId, String model, double baseRentalRate){
+        super(vehicleId, model, baseRentalRate);
+    }
+
+    @Override
+    public double calculateRent(int days) {
+        return getBaseRentalRate() * days;
+    }
 }

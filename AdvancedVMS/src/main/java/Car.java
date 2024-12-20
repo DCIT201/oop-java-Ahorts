4ae2bce5acb03d6727b3c5c50b702325d5beb21f
Ahorts <godswayahorts@gmail.com>
2024-12-20 11:28:00 +0000

    feat: Add AC and rent calculations

--- AdvancedVMS/src/main/java/Car.java
+++ AdvancedVMS/src/main/java/Car.java
@@ -1,10 +1,22 @@
 public class Car extends Vehicle {
+    private boolean hasAirConditioners;
+
     public Car(String vehicleId, String model, double baseRentalRate){
         super(vehicleId, model, baseRentalRate);
     }
 
+    public void setHasAirConditioners(boolean hasAirConditioners){
+        this.hasAirConditioners = hasAirConditioners;
+    }
+    public boolean getHasAirConditioners(){
+        return this.hasAirConditioners;
+    }
+
     @Override
     public double calculateRent(int days) {
+        if (hasAirConditioners){
+            return getBaseRentalRate() * days * 1.5;
+        }
         return getBaseRentalRate() * days;
     }
 }

e35c6278ea5c9c476eb50ac9399bd4f469d7fcef
Ahorts <godswayahorts@gmail.com>
2024-12-21 19:51:57 +0000

    feat: Write basic tests

--- AdvancedVMS/src/test/java/RentalTransactionTest.java
+++ AdvancedVMS/src/test/java/RentalTransactionTest.java
@@ -1,6 +1,25 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
 
-public class RentalTransactionTest {
+ class RentalTransactionTest {
+   private RentalTransaction rentalTransaction;
+   private Car car;
+   private Customer customer;
+
+   @BeforeEach
+  void setUp() {
+    car = new Car("CAR001", "BMW M5", 200);
+    customer = new Customer("CUST001", "John Doe", "jd@gmail.com");
+    rentalTransaction = new RentalTransaction("TRANS001", customer, car, LocalDate.of(2024,12,7), 5);
+   }
+   @Test
+  void testRentDate(){
+    assertEquals(LocalDate.of(2024, 12, 7), rentalTransaction.getRentDate());
+   }
+   @Test
+  void testReturnDate(){
+    assertEquals(LocalDate.of(2024,12, 12), rentalTransaction.getReturnDate());
+   }
 }

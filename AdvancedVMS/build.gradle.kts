9990d0212f44cc70288e4051a0f9378d7d8b00c5
Ahorts <godswayahorts@gmail.com>
2024-12-30 14:52:14 +0000

    feat: Add annotations

--- AdvancedVMS/build.gradle.kts
+++ AdvancedVMS/build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.jetbrains:annotations:16.0.2")
+
 }
 
 tasks.test {
